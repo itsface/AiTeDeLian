98a63146c84ee98258236c4106b767059cfbb474
Bushiczl <32673543+Bushiczl@users.noreply.github.com>
2018-05-17 14:59:55 +0800

    评论和状态的接口

--- IT_show/base/urls.py
+++ IT_show/base/urls.py
@@ -3,5 +3,7 @@ from .views import *
 
 urlpatterns = [
     url(r'^comment/get$', api_comment_get, name='api_comment_get'),
+    url(r'^comment/get2$', api_comment_get2, name='api_comment_get2'),
     url(r'^comment/submit$', api_comment_submit, name='api_comment_submit'),
+    url(r'^status/get', api_status_get, name='api_status_get'),
 ]
--- IT_show/base/views.py
+++ IT_show/base/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.core import serializers
 from show.models import Comment, HeadPicture
+from user.models import Fresher, StatusDetails
 import logging
 
 
@@ -31,9 +32,36 @@ def api_comment_get(request):
                 "createTime": c.createTime.strftime("%Y-%m-%d %H:%M:%S"),
                 "code": c.code,
             }
-        back["comment"] = comment
     except:
         back["success"] = False
+    return HttpResponse(json.dumps(back))
+
+
+def api_comment_get2(request):
+    back = []
+    try:
+        oneTimeGet = 10
+        code = int(request.GET.get("code", 0))
+        if code == 0:
+            comments = Comment.objects.all()
+        else:
+            comments = Comment.objects.filter(code__range=(0, code - 1))
+        comments = comments.order_by("-code")
+        len = comments.count()
+        if len > oneTimeGet:
+            len = oneTimeGet
+        comments = comments[0:len]
+        i = 0
+        for c in comments:
+            i = i + 1
+            back += [{
+                "head": c.head.pic.url,
+                "content": c.content,
+                "createTime": c.createTime.strftime("%Y-%m-%d %H:%M:%S"),
+                "code": c.code,
+            }]
+    except:
+        pass
     return HttpResponse(json.dumps(back))
 
 
@@ -52,4 +80,22 @@ def api_comment_submit(request):
         c.save()
     except:
         back["success"] = False
+    return HttpResponse(json.dumps(back))
+
+
+def api_status_get(request):
+    back = []
+    try:
+        userCode = request.GET.get("userCode", "")
+        # logging.debug(userCode)
+        statusList = StatusDetails.objects.filter(hostID__userCode=userCode)
+        # logging.debug(statusList)
+        for status in statusList:
+            back += [{
+                "statusName": status.statu.info,
+                "statusHappenTime": status.time.strftime("%Y-%m-%d %H:%M:%S"),
+                "statusOrder": status.code,
+            }]
+    except:
+        pass
     return HttpResponse(json.dumps(back))
